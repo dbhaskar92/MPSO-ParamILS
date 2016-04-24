7cf2edbfec9a0e4444ebd7396a8b76d1398122af
Dhananjay Bhaskar <dhananjay.bhaskar.92@gmail.com>
2016-04-23 23:38:28 -0700

    Removed tmp output files and added concat script

---
+++ paramils-output/concat_output.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+
+import glob
+import os
+
+path = "./mpso_output_*.txt"
+
+ofname = "./output_log.txt"
+
+if os.path.isfile(ofname):
+	print 'Error: Output file already exists'
+
+ofile = open(ofname,'w')
+
+file_list = glob.glob(path)
+file_list.sort(key=os.path.getmtime)
+
+for fname in file_list:
+
+	fd = os.path.basename(fname)
+	ofile.write(fname+"\n")
+	
+	with open(fd) as ifile:
+		for line in ifile:
+			ofile.write(line)
+			
+	ofile.write("\n\n")
+	
+	os.remove(fname)
+	
+ofile.close()
